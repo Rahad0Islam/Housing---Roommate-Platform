0fd127684a3ecbb5ed1127b9c8dd6020af318fa6
Rahad0Islam <vairahad99@gmail.com>
2026-09-04 19:26:49 +0600

    googleAuth setup complete

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -35,6 +35,9 @@ const config = {
 	bkash_app_key: process.env.BKASH_APP_KEY!,
 	bkash_app_secret: process.env.BKASH_APP_SECRET!,
 	bkash_callback_url: process.env.BKASH_CALLBACK_URL!,
+
+
+	google_client_id: process.env.GOOGLE_CLIENT_ID!,
 };
 
 export default config;
